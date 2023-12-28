6579c26048da40107d24bcb4adfc4da17b4d2e09
lizabethnt <lizabethnt@gmail.com>
2023-12-28 09:36:56 -0800

    add coments on the function's purpose

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,3 @@
 #main script for PyBank challenge
 #Totals number of months of data, amount of profit/losses, greatest increase and greatest decrease among bank records
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,5 @@
 #main file for PyPoll challenge
+#totals votes cast, prints list of candidates who recieved votes, 
+#percentage of votes for each candidate,
+#total number of votes for each candidate, and
+#The winner by popular vote
