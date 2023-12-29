da8e276df657ef2adcc359ada42234cf1a000b2d
lizabethnt <lizabethnt@gmail.com>
2023-12-28 16:36:49 -0800

    first code added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,23 @@
 #main script for PyBank challenge
 #Totals number of months of data, amount of profit/losses, greatest increase and greatest decrease among bank records
 
+import os
+import csv
+
+#get csv file
+budget_data_csv = os.path.join("Resources", "budget_data.csv")
+
+#use csv file to calculate number of months of data
+with open(budget_data_csv) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+    next(csv_reader)
+    monthscounter = 0
+    total = 0
+    for row in csv_reader:
+        monthscounter = monthscounter +1
+        total = total + int(row[1])
+#print results
+print("\n Financial Analysis \n--------------------------------")
+print(f"\nTotal Months: ", monthscounter)
+print(f"\nTotal: ${total}")
+
