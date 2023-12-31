04030114e5067b3459bac6b476433d65dcc3ae4f
lizabethnt <lizabethnt@gmail.com>
2023-12-31 15:30:11 -0800

    program completely functional

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,10 +8,12 @@ import csv
 total = 0
 prev_row_profit = 0
 avg_change = 0
-greatest_profit = 0
+greatest_increase = 0
 greatest_decrease = 0
 months_counter = 0
 change = []
+dates = []
+change_counter = 0
 
 #get csv file
 budget_data_csv = os.path.join("Resources", "budget_data.csv")
@@ -35,16 +37,32 @@ with open(budget_data_csv) as csvfile:
 #setup for next row's calculation
         prev_row_profit = int(row[1])
 
-#find the average change:need to first remove initial month's change list entry as it should be N/A
+#store the dates column as a list
+        dates.append(row[0])
+
+#find the greatest increase and associated date
+for value in change:
+        if value > greatest_increase:
+                greatest_increase = value
+                greatest_increase_date = dates[change_counter]
+
+        if value < greatest_decrease:
+                greatest_decrease = value
+                greatest_decrease_date = dates[change_counter]
+        
+        change_counter += 1
+        
+#find the average change:first remove initial month's change list entry as it should be N/A
 del change[0]
 avg_change = sum(change)/(len(change))
 
 #print results
-print(change)
 print("\n Financial Analysis \n------------------------")
 print(f"\nTotal Months: ", months_counter)
 print(f"\nTotal: ${total}")
 print(f"\nAverage Change: ${round(avg_change,2)}")
+print(f"\n\nGreatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
+print(f"\n\nGreatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
 
 # Set variable for output file
 output_file = os.path.join("python_challenge_final.txt")
@@ -55,3 +73,5 @@ with open(output_file, "w") as textfile:
     textfile.write(f"\n\nTotal Months: {months_counter}")
     textfile.write(f"\n\nTotal: ${total}")
     textfile.write(f"\n\nAverage Change: ${round(avg_change,2)}")
+    textfile.write(f"\n\nGreatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
+    textfile.write(f"\n\nGreatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
