57f7bb7cd9e94e3bda65386a8529659770f4846b
lizabethnt <lizabethnt@gmail.com>
2023-12-31 16:54:45 -0800

    start work in progress PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,22 @@
 #main file for PyPoll challenge
-#totals votes cast, prints list of candidates who recieved votes, 
-#percentage of votes for each candidate,
-#total number of votes for each candidate, and
-#The winner by popular vote
+import os
+import csv
+
+#declare and initialize variables
+votes = 0
+candidates = []
+percentage_votes = []
+candidate_votes = []
+winner = " "
+
+#get CSV file
+election_data_csv = os.path.join("Resources", "election_data.csv")
+
+#open CSV file
+with open(election_data_csv) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+    next(csv_reader)
+    for row in csv_reader:
+        for candidate in candidates:
+            if row[2] == candidate:
+                candidates.append(row[2])
